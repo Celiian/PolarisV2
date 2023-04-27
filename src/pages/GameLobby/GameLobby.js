bdf7f7d16e25007dd5cf66c5e016bfc35f726717
Jerance <michael.jerance@gmail.com>
2023-04-27 10:08:35 +0200

    FIX/Merge conflict home & gamelobby

--- src/pages/GameLobby/GameLobby.js
+++ src/pages/GameLobby/GameLobby.js
@@ -69,7 +69,9 @@ const GameLobby = () => {
   };
 
   const copyInviteLink = () => {
-    let linkInvite = `${window.location.origin}/lobby?room=${localStorage.getItem("room_token")}1`;
+    let linkInvite = `${
+      window.location.origin
+    }/lobby?room=${localStorage.getItem("room_token")}1`;
     navigator.clipboard.writeText(linkInvite).then(
       () => {
         console.log("Lien copié dans le presse-papiers :", linkInvite);
@@ -82,8 +84,14 @@ const GameLobby = () => {
 
   const startGame = async () => {
     const map = generate_map(40, roomData.players);
-    await setDataInDatabase(map, path + localStorage.getItem("room_token") + "/map/");
-    await setDataInDatabase(true, path + localStorage.getItem("room_token") + "/started/");
+    await setDataInDatabase(
+      map,
+      path + localStorage.getItem("room_token") + "/map/"
+    );
+    await setDataInDatabase(
+      true,
+      path + localStorage.getItem("room_token") + "/started/"
+    );
     window.location.href = "/map";
   };
 
@@ -134,52 +142,57 @@ const GameLobby = () => {
             </div>
           </div>
         </div>
-      </div>
-      {showModal === true && (
-        <div className="fixed z-10 inset-0 overflow-y-auto">
-          <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
-            <div className="fixed inset-0 transition-opacity">
-              <div className="absolute inset-0 bg-gray-800 opacity-75"></div>
-            </div>
-            <span className="hidden sm:inline-block sm:align-middle sm:h-screen"></span>
-            &#8203;
-            <div
-              className="inline-block align-bottom bg-gray-800 rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full sm:p-6"
-              role="dialog"
-              aria-modal="true"
-              aria-labelledby="modal-headline"
-            >
-              <div>
-                <div className="mt-3 text-center sm:mt-5">
-                  <h3 className="text-lg leading-6 font-medium text-white" id="modal-headline">
-                    Entrez votre nom pour rejoindre la partie
-                  </h3>
-                  <div className="mt-2">
-                    <div className="mb-4">
-                      <input
-                        type="text"
-                        id="name"
-                        value={namePlayer}
-                        onChange={(e) => setNamePlayer(e.target.value)}
-                        className="w-full px-3 py-2 border border-gray-700 rounded-md bg-gray-900 text-white focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
-                        placeholder="Entrez votre nom"
-                      />
-                    </div>
-                    <div className="flex justify-center">
-                      <button
-                        onClick={submitFormJoin}
-                        className="bg-purple-600 text-white px-4 py-2 rounded-md hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-opacity-50"
-                      >
-                        Rejoindre la partie
-                      </button>
+        {showModal === true && (
+          <div className="fixed z-10 inset-0 overflow-y-auto">
+            <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+              <div className="fixed inset-0 transition-opacity">
+                <div className="absolute inset-0 bg-gray-800 opacity-75"></div>
+              </div>
+              <span className="hidden sm:inline-block sm:align-middle sm:h-screen"></span>
+              &#8203;
+              <div
+                className="inline-block align-bottom bg-gray-800 rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full sm:p-6"
+                role="dialog"
+                aria-modal="true"
+                aria-labelledby="modal-headline"
+              >
+                <div>
+                  <div className="mt-3 text-center sm:mt-5">
+                    <h3
+                      className="text-lg leading-6 font-medium text-white"
+                      id="modal-headline"
+                    >
+                      Join GameRoom
+                    </h3>
+                    <div className="mt-2">
+                      <form onSubmit={submitFormJoin}>
+                        <div className="mb-4">
+                          <input
+                            type="text"
+                            id="name"
+                            value={namePlayer}
+                            onChange={(e) => setNamePlayer(e.target.value)}
+                            className="w-full px-3 py-2 border border-gray-700 rounded-md bg-gray-900 text-white focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
+                            placeholder="Enter name"
+                          />
+                        </div>
+                        <div className="flex justify-center">
+                          <button
+                            type="submit"
+                            className="bg-purple-600 text-white px-4 py-2 rounded-md hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-opacity-50"
+                          >
+                            Join
+                          </button>
+                        </div>
+                      </form>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-      )}
+        )}
+      </div>
     </>
   );
 };
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -48,7 +48,7 @@ const Home = () => {
   localStorage.clear();
 
   return (
-    <div>
+    <>
       <div className="h-screen">
         <div className="video-wrapper">
           <video autoPlay loop muted src={BackGroundVideo}></video>
@@ -63,40 +63,55 @@ const Home = () => {
                 <h2 className="text-center" style={{ color: "white" }}>
                   Create GameRoom
                 </h2>
-                <div className="mb-4">
-                  <input
-                    type="text"
-                    id="name"
-                    value={nameOwnerPlayer}
-                    onChange={(e) => setNameOwnerPlayer(e.target.value)}
-                    className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent dark:bg-gray-700 dark:border-gray-600 dark:text-white"
-                    placeholder="Enter name"
-                  />
-                </div>
-                <button onClick={() => submitFormCreate()} className="custom-button">
-                  Create
-                </button>
+                <form
+                  className="flex flex-col items-center h-full justify-center"
+                  onSubmit={submitFormCreate}
+                >
+                  <div className="mb-4">
+                    <input
+                      type="text"
+                      id="name"
+                      value={nameOwnerPlayer}
+                      onChange={(e) => setNameOwnerPlayer(e.target.value)}
+                      className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent dark:bg-gray-700 dark:border-gray-600 dark:text-white"
+                      placeholder="Enter name"
+                    />
+                  </div>
+                  <button type="submit" className="custom-button">
+                    Create
+                  </button>
+                </form>
               </div>
             </div>
+
             <div className="container-rules">
               <div className="bg-gray-800 rounded-md shadow-md p-4">
-                <h2 className="text-lg font-bold mb-2 text-white">Règles du jeu</h2>
+                <h2 className="text-lg font-bold mb-2 text-white">
+                  Règles du jeu
+                </h2>
                 <ul className="list-disc list-inside text-white p-3">
                   <li>Maximum 4 players to play a game</li>
-                  <li>Players collect resources to build ships, space stations ships, space stations ...</li>
-                  <li>Players can build equipped spaceshipsto explore, conquer.</li>
                   <li>
-                    Players can build mining stations to collect resources, produce energy and
+                    Players collect resources to build ships, space stations ships, space stations ...
+                  </li>
+                  <li>
+                    Players can build equipped spaceshipsto explore, conquer.
+                  </li>
+                  <li>
+                    Players can build mining stations to collect resources,
+                    produce energy and
                     <br />
                     resources, produce energy and protect their territory.
                   </li>
                   <li>
-                    Players invest in advanced technologies to improve their spaceships
+                    Players invest in advanced technologies to improve their
+                    spaceships
                     <br />
                     improve their spaceships, bases and miners.
                   </li>
                   <li>
-                    Players send ships to explore new territories, discover resources and find
+                    Players send ships to explore new territories, discover
+                    resources and find
                     <br />
                     discover resources and find rare ores.
                   </li>
@@ -106,14 +121,17 @@ const Home = () => {
                     rare minerals.
                   </li>
                   <li>
-                    Victory Condition: The player who first reaches the total of 10 victory points wins.
+                    Victory Condition: The player who first reaches the total of
+                    10 victory points wins.
                     <br />
                     total of 10 victory points wins the game
                   </li>
                   <li>
-                    Players can gain victory points by building space elements (bases, ships, miners), <br></br>
+                    Players can gain victory points by building space elements
+                    (bases, ships, miners), <br></br>
                     discovering resources and resources and minerals, <br></br>
-                    and by trading resources, technology and minerals with other players.
+                    and by trading resources, technology
+                    and minerals with other players.
                   </li>
                 </ul>
               </div>
@@ -121,8 +139,8 @@ const Home = () => {
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
-};
+}
 
 export default Home;
