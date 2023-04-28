b433d5593be41b9ebf08a64df4eb826984a41b9a
Jerance <michael.jerance@gmail.com>
2023-04-28 13:02:58 +0200

    FEAT/Rework trade modal

--- src/components/TradeModal/TradeModal.js
+++ src/components/TradeModal/TradeModal.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import "./TradeModal.css";
 
 // Ressources
@@ -8,11 +10,20 @@ import uranium from "../../assets/img/ressources/mine/uranium.png";
 import coins from "../../assets/img/ressources/coins.png";
 import ore from "../../assets/img/ressources/mine/iron.png";
 import crystal from "../../assets/img/ressources/mine/crystal.png";
+import ship_part from "../../assets/img/ressources/ship_part.png";
+
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormHelperText from "@mui/material/FormHelperText";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import { TextField } from "@mui/material";
 
 const ressourceImages = {
   water: water,
   foodCan: food_can,
   shipEngine: ship_engine,
+  shipPart: ship_part,
   uranium: uranium,
   coins: coins,
   ore: ore,
@@ -20,58 +31,125 @@ const ressourceImages = {
 };
 
 const TradeModal = ({ showModal, handleClose, ressources, handleTrade }) => {
+  const [ressourceSelected, setRessourceSelected] = useState("");
+  const [tradeSelected, setTradeSelected] = useState("");
+  const [showQuantityRessources, showQuantitySelected] = useState(false);
+
+  const handleChangeRessources = (event) => {
+    setRessourceSelected(event.target.value);
+    showQuantitySelected(true);
+  };
+
+  const handleChangeTrades = (event) => {
+    setTradeSelected(event.target.value);
+  };
+
   return (
     <>
       {showModal && (
         <div className="fixed inset-0 bg-gray-900 bg-opacity-50 flex justify-center items-center">
-          <div className="bg-gray-700 rounded-md shadow-md p-4 w-4/5 max-w-lg">
-            <div className="grid grid-cols-2 gap-4">
+          <div className="bg-gray-700 rounded-md shadow-md p-4 w-4/5 max-w-lg h-72">
+            <div className="grid grid-cols-2 gap-4 h-full">
               <div className="bg-gray-700 rounded-md shadow-md p-4">
                 <h3 className="text-lg font-bold mb-2 text-white">
                   Your ressources
                 </h3>
                 <div className="flex flex-row flex-wrap items-center gap-2 text-white">
-                  {Object.entries(ressources).map(([key, value]) => (
-                    <div className="resources" key={key}>
-                      <img
-                        className="ressource-img"
-                        src={ressourceImages[key]}
-                        alt={key}
-                      />
-                      <p className="ressources-value">
-                        {key} : {value}
-                      </p>
-                    </div>
-                  ))}
+                  <FormControl sx={{ m: 1, minWidth: 120 }}>
+                    <Select
+                      value={ressourceSelected}
+                      onChange={handleChangeRessources}
+                      displayEmpty
+                      inputProps={{ "aria-label": "Without label" }}
+                    >
+                      {Object.entries(ressources).map(([key]) => (
+                        <MenuItem key={key} value={key}>
+                          <img
+                            className="ressource-img"
+                            src={ressourceImages[key]}
+                            alt={key}
+                          />
+                          {key}
+                        </MenuItem>
+                      ))}
+                    </Select>
+                  </FormControl>
+                  {showQuantityRessources === true ? (
+                    <p>
+                      You have {ressources[ressourceSelected]}{" "}
+                      {ressourceSelected}
+                    </p>
+                  ) : (
+                    <div></div>
+                  )}
                 </div>
               </div>
-              <div className="bg-gray-700 rounded-md shadow-md p-4">
-                <div className="trade-main-container">
-                  <h3 className="text-lg font-bold text-white">Trades</h3>
+              <div className="bg-gray-700 rounded-md shadow-md p-4 h-full">
+                <div className="flex justify-between">
+                  <h3 className="text-lg font-bold mb-2 text-white">Trades</h3>
                   <button className="btn-close-trade" onClick={handleClose}>
                     X
                   </button>
                 </div>
 
-                <div className="flex flex-col gap-4">
-                  <div className="flex flex-row items-center justify-between bg-gray-800 rounded-md p-2">
-                    <div className="flex flex-row items-center">
-                      <img
-                        src={ressourceImages.water}
-                        alt="Water"
-                        className="w-8 h-8 mr-2"
-                      />
-                      <span className="text-white font-bold">5 Water</span>
-                    </div>
-                    <div className="text-white font-bold">3 coins</div>
-                  </div>
-                  <button
-                    className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded-md"
-                    onClick={handleTrade}
-                  >
-                    Trade
-                  </button>
+                <div className="trade-main-container">
+                  {ressourceSelected === "coins" ? (
+                    <FormControl sx={{ m: 1, minWidth: 120 }}>
+                      <Select
+                        value={tradeSelected}
+                        onChange={handleChangeTrades}
+                        displayEmpty
+                        inputProps={{ "aria-label": "Without label" }}
+                      >
+                        {Object.entries(ressources)
+                          .filter(([key]) => key !== "coins")
+                          .map(([key]) => (
+                            <MenuItem key={key} value={key}>
+                              <img
+                                className="ressource-img"
+                                src={ressourceImages[key]}
+                                alt={key}
+                              />
+                              {key}
+                            </MenuItem>
+                          ))}
+                      </Select>
+                    </FormControl>
+                  ) : (
+                    <>
+                      <div
+                        className="coins-trading-container"
+                        style={{ padding: "10px" }}
+                      >
+                        <TextField
+                          id="outlined-basic"
+                          label="CoinsNumber"
+                          inputProps={{ type: "number" }}
+                          variant="outlined"
+                        />
+                        <div className="coins-container">
+                          <img
+                            className="ressource-img"
+                            src={ressourceImages.coins}
+                            alt={"coins"}
+                          />
+                          <p>Coins</p>
+                        </div>
+                      </div>
+                      <div className="pt-5">
+                        <p style={{ color: "white", fontWeight: "bold" }}>
+                          Remaining coins: {}
+                        </p>
+                      </div>
+                    </>
+                  )}
                 </div>
+                <button
+                  className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded-md"
+                  onClick={handleTrade}
+                >
+                  Trade
+                </button>
               </div>
             </div>
           </div>
